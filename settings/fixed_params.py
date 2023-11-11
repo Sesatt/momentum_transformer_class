74a67c62dbd8bb69cf650df17499f178eb22feaa
Sesatt <67186636+Sesatt@users.noreply.github.com>
2023-11-11 13:28:42 +0100

    Update fixed_params.py

--- settings/fixed_params.py
+++ settings/fixed_params.py
@@ -1,7 +1,7 @@
 MODLE_PARAMS = {
     "architecture": "TFT",
     "total_time_steps": 252,
-    "early_stopping_patience": 10,
+    "early_stopping_patience": 15,
     "multiprocessing_workers": 32,
     "num_epochs": 200,
     "fill_blank_dates": False,
